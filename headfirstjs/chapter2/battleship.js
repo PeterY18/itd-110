a84d03ae1642fbccf8452121f0e81e548f19b5ba
PeterY18 <peter.yuan18@gmail.com>
2019-11-26 09:28:22 -0500

    almost finished battleships and chapter2 notes

--- headfirstjs/chapter2/battleship.js
+++ headfirstjs/chapter2/battleship.js
@@ -14,11 +14,18 @@ while (isSunk == false) {
         alert("Please enter a valid cell number!");
     } else {
         guesses = guesses + 1;
+    
+        if (guess == location1 || guess == location2 || guess == location3) {
+            hits = hits + 1;
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sank my battle ship!");
+            }
+        } else {
+            alert("MISS");
+        }
     }
-    if (guess == location1 || guess == location2 || guess == location3) {
-        hits = hits + 1;
-    }
-    if (hits == 3) {
-        isSunk = true;
-        alert("You sank my battle ship!");
-    }
+}
+var stats = "You took " + guesses + " guesses to sink the battleship, " +
+"which means your shooting accuracy was " + (3/guesses);
+alert(stats);
